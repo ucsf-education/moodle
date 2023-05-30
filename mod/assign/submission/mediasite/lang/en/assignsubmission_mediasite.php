43c58601d4bf2951b97658a6ccb085937991bc93
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2023-05-30 07:38:50 -0700

    adds Mediasite plugins.
    code copied from
    https://github.com/SonicFoundry/Mediasite-Content-Module-for-Moodle,
    at commit hash 93673bc8c6f6c417edc7c1656754fafd46656be7 (Oct 6, 2020)

---
+++ mod/assign/submission/mediasite/lang/en/assignsubmission_mediasite.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Mediasite plugin for Moodle.
+ *
+ * @package assignsubmission_mediasite
+ * @copyright Sonic Foundry 2017  {@link http://sonicfoundry.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['default'] = 'Enable by default';
+$string['default_help'] = 'If set, this submission method will be enabled by default for all new assignments.';
+$string['emptycontenterrormessage'] = 'No video submission found.';
+$string['enabled'] = 'Mediasite Content(for enabled)';
+$string['enabled_help'] = "If enabled, students are able to submit Mediasite Content for assessment in Moodle.";
+$string['launchbuttontext'] = 'Upload Media';
+$string['numwordsforlog'] = 'Submission word count: {$a} words';
+$string['pluginname'] = 'Mediasite Assignment Submission';
+$string['privacy:metadata'] = 'The Mediasite assignment submission plugin only displays existing Mediasite data.';
+$string['submissiontext'] = 'Video Submission';
+$string['submissiontype'] = 'Video Assignment';
